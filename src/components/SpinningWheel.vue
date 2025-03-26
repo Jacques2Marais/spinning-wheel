6ad7e0189829757d874436ca56017619166b4b9e
Jacques Marais <iamjacquesmarais@gmail.com>
2025-03-26 10:35:37 +0200

    Implement random and predetermined spinning with animation on wheel

--- src/components/SpinningWheel.vue
+++ src/components/SpinningWheel.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useTemplateRef } from 'vue'
+
 // given the segment's index, calculate its CSS transformation property (i.e. rotation)
 const calculateSegmentTransformation = (index: number) => {
   const rotation = (index - 1) * 45
@@ -30,35 +32,60 @@ const segmentPath = `M 0,0
   L ${segmentLength},0
   A ${segmentLength},${segmentLength} 0 0,1 ${segmentArcEndX},${segmentArcEndY}
 Z`
+
+const wheel = useTemplateRef('spinner-wheel')
+
+// always spin 10 full circles to create a "spinning effect"
+const spinEffectDegrees = 360 * 10
+
+const getRandomSpinDegrees = () => {
+  const randomNumber = Math.random()
+  const randomSegment = Math.floor(randomNumber * 8) + 1
+  const randomRotationDegrees = spinEffectDegrees + (randomSegment - 1) * 45 + 22.5
+
+  return randomRotationDegrees
+}
+
+const getPredeterminedSpinDegrees = () => {
+  const predeterminedSegment = 3
+  const predeterminedRotationDegrees = spinEffectDegrees + (360 + 22.5 - predeterminedSegment * 45)
+
+  return predeterminedRotationDegrees
+}
+
+const spin = (degrees: number) => {
+  if (wheel.value) {
+    wheel.value.style.transform = `rotate(${degrees}deg)`
+  }
+}
 </script>
 
 <template>
   <div class="wheel-container">
-    <svg width="32" viewBox="0 0 30 30" class="spinner-arrow">
-      <polygon points="30,0 0,0 15,30" />
+    <svg width="40" viewBox="0 0 30 30" class="spinner-arrow">
+      <polygon points="28,2 2,2 15,28" />
     </svg>
 
-    <svg width="300" height="300" viewBox="-2 -2 204 204">
+    <svg width="300" height="300" viewBox="-2 -2 204 204" class="spinner-wheel" ref="spinner-wheel">
       <defs>
         <path id="segment" :d="segmentPath" />
 
-        <linearGradient id="segment-gradient-1">
+        <linearGradient id="segment-gradient-1" gradientTransform="rotate(60)">
           <stop class="gradient-stop-1" offset="0%" />
           <stop class="gradient-stop-2" offset="100%" />
         </linearGradient>
 
-        <linearGradient id="segment-gradient-2">
+        <linearGradient id="segment-gradient-2" gradientTransform="rotate(90)">
           <stop class="gradient-stop-1" offset="0%" />
           <stop class="gradient-stop-2" offset="100%" />
         </linearGradient>
 
-        <linearGradient id="segment-gradient-3">
+        <linearGradient id="segment-gradient-3" gradientTransform="rotate(30)">
           <stop class="gradient-stop-1" offset="0%" />
           <stop class="gradient-stop-2" offset="100%" />
         </linearGradient>
 
-        <!-- radial gradient -->
-        <radialGradient id="middle-gradient" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+        <radialGradient id="middle-gradient">
           <stop class="gradient-stop-1" offset="0%" />
           <stop class="gradient-stop-2" offset="100%" />
         </radialGradient>
@@ -86,7 +113,14 @@ Z`
       </g>
 
       <circle cx="100" cy="100" r="16" class="spinner-middle" />
+      <circle cx="100" cy="100" r="100" class="spinner-edge" />
     </svg>
+  </div>
+  <div class="button-group">
+    <button class="button button-secondary" @click="spin(getPredeterminedSpinDegrees())">
+      Predetermined spin
+    </button>
+    <button class="button button-primary" @click="spin(getRandomSpinDegrees())">Random spin</button>
   </div>
 </template>
 
@@ -97,21 +131,32 @@ Z`
   align-items: center;
 }
 
+.spinner-wheel {
+  transition: transform 5s cubic-bezier(0.3, 0.1, 0.25, 1);
+  transform-origin: 50% 50%;
+  transform: rotate(0);
+}
+
 .spinner-arrow {
   fill: var(--foreground);
   translate: 0 0.75rem;
+  stroke: var(--foreground);
+  stroke-width: 4;
+  stroke-linejoin: round;
+
+  z-index: 1;
 }
 
 .spinner-text {
   font-size: 14px;
-  font-weight: 500;
-  fill: var(--foreground);
+  font-weight: 800;
+  fill: var(--background);
 }
 
 .spinner-segment {
   fill: url(#segment-gradient-1);
-  stroke: var(--foreground);
-  stroke-width: 2;
+  stroke: var(--background);
+  stroke-width: 4;
   stroke-linejoin: round;
 
   &.segment-1\/3 {
@@ -123,10 +168,16 @@ Z`
   }
 }
 
+.spinner-edge {
+  fill: none;
+  stroke: var(--background);
+  stroke-width: 4;
+}
+
 .spinner-middle {
   fill: url(#middle-gradient);
-  stroke: var(--foreground);
-  stroke-width: 2;
+  stroke: var(--background);
+  stroke-width: 4;
   stroke-linejoin: round;
 }
 
--- src/views/SpinView.vue
+++ src/views/SpinView.vue
@@ -6,9 +6,5 @@ import SpinningWheel from '@/components/SpinningWheel.vue'
 <template>
   <BaseLayout title="Choose how to spin" desc="Choose below how you would like to spin the wheel">
     <SpinningWheel />
-    <div class="button-group">
-      <button class="button button-secondary">Predetermined spin</button>
-      <button class="button button-primary">Random spin</button>
-    </div>
   </BaseLayout>
 </template>
